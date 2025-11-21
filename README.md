0b90acb2fd9b80a7b8a23b3093470d5f8bd898e2
William Woodruff <william@astral.sh>
2025-11-21 15:15:35 -0500

    Initial implementation
    Signed-off-by: William Woodruff <william@astral.sh>

---
+++ README.md
@@ -0,0 +1,120 @@
+# attest-action
+
+A GitHub Action that generates [PEP 740] publish attestations for your Python
+packages.
+
+[PEP 740]: https://peps.python.org/pep-0740/
+
+To use this action, you should be using [Trusted Publishing] to publish your
+packages (to [pyx], [PyPI], or another compatible index).
+
+[Trusted Publishing]: https://docs.pypi.org/trusted-publishers/
+
+[pyx]: https://pyx.dev
+
+[PyPI]: https://pypi.org
+
+> [!IMPORTANT]
+> This action is primarily useful for directly publishing with [`uv publish`]
+> and other upload tools that support PEP 740 attestations directly.
+> You **do not need** this action if you're using [pypa/gh-action-pypi-publish],
+> as that action has built-in support for PEP 740 attestations.
+
+[`uv publish`]: https://docs.astral.sh/uv/guides/package/
+
+[pypa/gh-action-pypi-publish]: https://github.com/pypa/gh-action-pypi-publish
+
+## Contents
+
+- [Usage](#usage)
+  - [Prerequisites](#prerequisites)
+  - [Quickstart](#quickstart)
+- [Inputs](#inputs)
+    - [`paths`](#paths)
+- [Outputs](#outputs)
+
+## Usage
+
+### Prerequisites
+
+To use this action, you must have a Trusted Publisher configured for your
+project on your target index (or indices). Refer to your index's documentation
+for more information on Trusted Publishing:
+
+- [pyx - Trusted Publishing](https://docs.pyx.dev/publishing#trusted-publishing)
+- [PyPI - Trusted Publishing](https://docs.pypi.org/trusted-publishers/)
+
+As with Trusted Publishing itself, this action requires the `id-token: write`
+permission on your publishing job. For example:
+
+```yaml
+permissions:
+  id-token: write # for Trusted Publishing + attest-action
+  contents: read # for actions/checkout, if you're in a private repo
+```
+
+### Quickstart
+
+Add `astral-sh/attest-action` directly above your publishing step in your
+publishing job. For example:
+
+```yaml
+jobs:
+  publish:
+    runs-on: ubuntu-latest
+    permissions:
+      id-token: write
+      contents: read
+
+    needs: [build]
+    steps:
+      - uses: actions/checkout@v6
+
+      # NOTE: Fetches the to-be-published distributions from a previous build job.
+      # Running builds in their own job is strongly encouraged for both
+      # security and reproducibility.
+      - uses: actions/download-artifact@v6
+        with:
+          name: dist
+
+      - uses: astral-sh/attest-action@v0.0.1
+
+      - run: uv publish
+```
+
+If you have a custom path (or paths) to your distributions, you can specify them
+via the `paths` input as whitespace-separated values.
+
+```yaml
+- uses: astral-sh/attest-action@v0.0.1
+  with:
+  paths: |
+    custom-dist-dir/*
+    wheelhouse/*
+```
+
+## Inputs
+
+### `paths`
+
+**Default:** `dist/*`
+
+One or more whitespace-separated directories or glob patterns to search for
+Python distributions to generate attestations for.
+
+Recursive globs may be used, e.g. `dist/**` to find all distributions within
+`dist/` and its subdirectories.
+
+> [!NOTE]
+> A bare directory path like `dist/` is treated as `dist/*`.
+
+### `overwrite`
+
+**Default:** `false`
+
+If `false` (the default), any existing publish attestations that *would* have
+been overwritten will instead cause the action to fail.
+
+If `true`, existing attestations will be overwritten.
+
+## Outputs
---
+++ action.py
@@ -0,0 +1,156 @@
+# /// script
+# requires-python = ">=3.14"
+# dependencies = [
+#     "pypi-attestations",
+#     "sigstore",
+# ]
+# ///
+
+import logging
+import os
+import shlex
+from pathlib import Path
+
+from pypi_attestations import Attestation, Distribution
+from sigstore import oidc
+from sigstore.models import ClientTrustConfig
+from sigstore.sign import SigningContext
+
+logger = logging.getLogger(__name__)
+
+
+def _get_input(name: str) -> str | None:
+    """
+    Get an action input from the environment, or `None` if not set.
+    """
+    env = f"ATTEST_ACTION_INPUT_{name.upper()}"
+    return os.getenv(env)
+
+
+def _get_path_patterns() -> list[str]:
+    """
+    Retrieve and normalize the 'paths' input.
+
+    Paths are split on whitespace (with shell lexing rules), and any bare directory
+    paths are normalized to include all files within that directory (i.e. `foo/`
+    becomes `foo/*`).
+    """
+    raw_paths = _get_input("paths")
+    if not raw_paths:
+        raise RuntimeError("Internal error: no 'paths' input provided")
+
+    paths = shlex.split(raw_paths)
+    if not paths:
+        raise RuntimeError("No paths provided in 'paths' input")
+
+    # Normalize `foo/` to `foo/*`
+    paths = [str(Path(p) / "*") if Path(p).is_dir() else p for p in paths]
+
+    return paths
+
+
+def _unroll_files(patterns: list[str]) -> list[Path]:
+    """
+    Given one or more path patterns (which may include glob patterns), unroll and
+    return all matching files.
+    """
+
+    files = []
+
+    for pattern in patterns:
+        for path in Path().glob(pattern):
+            if path.is_file():
+                files.append(path)
+
+    return files
+
+
+def _collect_dists(patterns: list[str]) -> list[tuple[Path, Distribution]]:
+    """
+    Given one or more path patterns (which may include glob patterns), collect and
+    return all Python distributions found at those paths.
+
+    A bare directory path like `foo/` is treated as `foo/*`, i.e.
+    all distributions within that directory.
+
+    Distributions are returned as a list of tuples, where each tuple contains
+    the `Path` to the distribution file and the corresponding `Distribution`
+    object.
+    """
+
+    files = _unroll_files(patterns)
+    dists = []
+
+    for file in files:
+        try:
+            dist = Distribution.from_file(file)
+            dists.append((file, dist))
+
+        except Exception as _:
+            logger.debug(f"skipping non-distribution file: {file}")
+            continue
+
+    return dists
+
+
+def _get_id_token() -> oidc.IdentityToken:
+    """
+    Obtain the ambient OIDC identity token.
+    """
+    id_token = oidc.detect_credential()
+
+    if not id_token:
+        raise RuntimeError("Failed to obtain OIDC identity token")
+
+    return oidc.IdentityToken(raw_token=id_token)
+
+
+def _attest(
+    dists: list[tuple[Path, Distribution]],
+    id_token: oidc.IdentityToken,
+    overwrite: bool = False,
+) -> None:
+    """
+    Generate and write PEP 740 publish attestations for the given distributions.
+
+    If `overwrite` is `False`, existing attestation files will not be overwritten
+    and an error will be raised instead.
+    """
+
+    # Before setting up any signing state, precompute the paths we intend
+    # to write attestations to (and fail if any already exist and overwrite
+    # is disabled).
+    dists_with_dests: list[tuple[Path, Distribution, Path]] = []
+    for file, dist in dists:
+        parent = file.parent
+        filename = file.name
+        attestation_name = f"{filename}.publish.attestation"
+        attestation_path = parent / attestation_name
+
+        if attestation_path.exists() and not overwrite:
+            raise RuntimeError(f"Attestation file already exists: {attestation_path}")
+
+        dists_with_dests.append((file, dist, attestation_path))
+
+    trust = ClientTrustConfig.production()
+    context = SigningContext.from_trust_config(trust)
+
+    with context.signer(identity_token=id_token) as signer:
+        for _, dist, attestation_path in dists_with_dests:
+            attestation = Attestation.sign(signer, dist)
+            attestation_path.write_text(attestation.model_dump_json())
+
+
+def main() -> None:
+    path_patterns = _get_path_patterns()
+
+    dists = _collect_dists(path_patterns)
+
+    id_token = _get_id_token()
+    overwrite = _get_input("overwrite") == "true"
+
+    _attest(dists, id_token, overwrite=overwrite)
+
+
+if __name__ == "__main__":
+    main()
